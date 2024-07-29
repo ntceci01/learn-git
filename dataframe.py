0145fec3d582db01714fc72c0b43655f5adc4d98
NTC <nathan@tkcecil.com>
2024-07-29 18:21:11 -0400

    deletions

--- dataframe.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({
-    'vertigo': ['crash', 'forever//over', 'gold'],
-    'no future': ['hertz', 'isohel', 'untitled'],
-    'ICYMI': ['Modern Warfare', 'PS1', 'Elsewhere']
-})
-
-print(df.to_string(index=False))
--- learn-git.py
+++
@@ -1,7 +0,0 @@
-print('futurebound')
-
-print('pathfinder')
-
-print('invisible lines')
-
-print('there are no ends just grays and half-lives')

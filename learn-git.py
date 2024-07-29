1e98e2833aa09ecac12ff3e6fd5ac8d674890c81
NTC <nathan@tkcecil.com>
2024-07-29 13:28:25 -0400

    End Credits

--- learn-git.py
+++ learn-git.py
@@ -1,3 +1,16 @@
+import pandas as pd
+
 print('futurebound')
 
 print('pathfinder')
+
+print('there are no ends just grays and half-lives')
+
+df = pd.DataFrame({
+    'vertigo': ['crash', 'forever//over', 'gold'],
+    'no future': ['hertz', 'isohel', 'untitled'],
+    'ICYMI': ['Modern Warfare', 'PS1', 'Elsewhere']
+})
+
+print(df.to_string(index=False))
+
